f1ab9f73908278e1e30a5c6c7c809998402825c0
JeanPierreLeon <jeanp.leons@uqvirtual.edu.co>
2025-09-29 09:54:13 -0500

    Creacion Base de DTO, DAO, Services, ServicesImpl

---
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/config/dto/services/SellerService.java
@@ -0,0 +1,9 @@
+package com.uniquindio.alojamientosAPI.config.dto.services;
+
+
+
+import com.uniquindio.alojamientosAPI.config.dto.SellerDTO;
+
+public interface SellerService {
+    SellerDTO getSellerById(Long id);
+}
---
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/entity/Product.java
@@ -0,0 +1,29 @@
+package com.uniquindio.alojamientosAPI.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+public class Product {
+
+    // Getters & Setters
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private Long sellerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
+
