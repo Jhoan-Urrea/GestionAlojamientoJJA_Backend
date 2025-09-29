d27e997656d2de8dd1f45672ba8198a68884e536
JeanPierreLeon <jeanp.leons@uqvirtual.edu.co>
2025-09-29 10:14:35 -0500

    Creacion SellerController

--- alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/config/dto/services/SellerService.java
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/config/dto/services/SellerService.java
@@ -1,9 +1,12 @@
 package com.uniquindio.alojamientosAPI.config.dto.services;
 
-
-
 import com.uniquindio.alojamientosAPI.config.dto.SellerDTO;
+import java.util.List;
 
 public interface SellerService {
+    SellerDTO createSeller(SellerDTO sellerDTO);
     SellerDTO getSellerById(Long id);
+    List<SellerDTO> getAllSellers();
+    SellerDTO updateSeller(Long id, SellerDTO sellerDTO);
+    void deleteSeller(Long id);
 }
