5fb16e7349850e1c3ed3b374cc00a3e4cfe3fefb
Jhoan Urrea <js.urrea03@gmail.com>
2025-10-12 10:14:34 -0500

    Se implementan los repositorios JPA del módulo Alojamiento

--- alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/AccommodationRepository.java
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/AccommodationRepository.java
@@ -1,5 +1,20 @@
 package com.uniquindio.alojamientosAPI.persistence.repository.accommodation;
 
-public class AccommodationRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.uniquindio.alojamientosAPI.persistence.entity.accommodation.AccommodationEntity;
+
+import java.util.List;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository<AccommodationEntity, Long> {
+
+    // Buscar alojamientos por ciudad
+    List<AccommodationEntity> findByCityId(Long cityId);
+
+    // Buscar alojamientos por usuario anfitrión
+    List<AccommodationEntity> findByHostUserId(Long hostUserId);
+
+    // Buscar alojamientos por estado
+    List<AccommodationEntity> findByStateAccommodationId(Long stateAccommodationId);
 }
