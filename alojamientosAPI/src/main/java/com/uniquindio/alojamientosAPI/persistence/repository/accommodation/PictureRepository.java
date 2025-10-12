5f06cd63d12291e3cd1f95e98589697031081065
Jhoan Urrea <js.urrea03@gmail.com>
2025-10-12 10:14:34 -0500

    Se implementan los repositorios JPA del módulo Alojamiento

--- alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/PictureRepository.java
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/PictureRepository.java
@@ -1,5 +1,18 @@
 package com.uniquindio.alojamientosAPI.persistence.repository.accommodation;
 
-public class PictureRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uniquindio.alojamientosAPI.persistence.entity.accommodation.PictureEntity;
+
+import java.util.List;
+
+@Repository
+public interface PictureRepository extends JpaRepository<PictureEntity, Long> {
+
+    // Obtener imágenes por alojamiento
+    List<PictureEntity> findByAccommodationId(Long accommodationId);
+
+    // Obtener imagen principal del alojamiento
+    PictureEntity findByAccommodationIdAndIsMainTrue(Long accommodationId);
 }
