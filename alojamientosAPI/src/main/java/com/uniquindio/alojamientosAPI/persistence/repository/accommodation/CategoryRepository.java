fc6ba27e71eebde25203673412d620061878c06b
Jhoan Urrea <js.urrea03@gmail.com>
2025-10-12 10:14:34 -0500

    Se implementan los repositorios JPA del módulo Alojamiento

--- alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/CategoryRepository.java
+++ alojamientosAPI/src/main/java/com/uniquindio/alojamientosAPI/persistence/repository/accommodation/CategoryRepository.java
@@ -1,5 +1,10 @@
 package com.uniquindio.alojamientosAPI.persistence.repository.accommodation;
 
-public class CategoryRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uniquindio.alojamientosAPI.persistence.entity.accommodation.CategoryEntity;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
